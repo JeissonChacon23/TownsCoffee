d85386651b43dc18e1810964ba00e0805c0f4cc5
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-06-21 17:42:30 -0500

    Initial Commit

---
+++ TownsCoffee/TownsCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TownsCoffeeApp.swift
+//  TownsCoffee
+//
+//  Created by Jeisson Chacón on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TownsCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
